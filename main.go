79580ade11315af7644a0b59716793ed7e003b6a
Pranjal Soni <soni.pranjals17@gmail.com>
2026-01-01 00:43:35 +0530

    Submit ch1, l11

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ func main() {
 	root := os.DirFS(".")
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServerFS(root))
-	mux.Handle("/assets/logo.png", http.FileServerFS(root))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServerFS(root)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := http.Server{
 		Addr:    ":8080",
