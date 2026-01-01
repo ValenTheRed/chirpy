efc9dac3a633653c0e6b3e4d27ee20945b2bbe8e
Pranjal Soni <soni.pranjals17@gmail.com>
2026-01-01 21:58:50 +0530

    Refactor: use HandlerFunc instead for /metrics and /reset

--- main.go
+++ main.go
@@ -19,24 +19,20 @@ func (cfg *apiConfig) increaseRequestsCount(handler http.Handler) http.Handler {
 	})
 }
 
-func (cfg *apiConfig) logRequestsCount() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		response := fmt.Sprintf(`<html>
+func (cfg *apiConfig) logRequestsCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	response := fmt.Sprintf(`<html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
     <p>Chirpy has been visited %v times!</p>
   </body>
 </html>`, cfg.requestsCount.Load())
-		w.Write(fmt.Append(nil, response))
-	})
+	w.Write(fmt.Append(nil, response))
 }
 
-func (cfg *apiConfig) resetRequestsCount() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.requestsCount.Swap(0)
-		w.Write([]byte("OK"))
-	})
+func (cfg *apiConfig) resetRequestsCount(w http.ResponseWriter, r *http.Request) {
+	cfg.requestsCount.Swap(0)
+	w.Write([]byte("OK"))
 }
 
 func main() {
@@ -53,8 +49,8 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.Handle("GET /admin/metrics", cfg.logRequestsCount())
-	mux.Handle("POST /admin/reset", cfg.resetRequestsCount())
+	mux.HandleFunc("GET /admin/metrics", cfg.logRequestsCount)
+	mux.HandleFunc("POST /admin/reset", cfg.resetRequestsCount)
 	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	server := http.Server{
